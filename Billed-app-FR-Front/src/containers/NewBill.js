0571804bf0023fe22b1269e76e79c2cb438c433d
eymj <jeremy.bourland@mailo.com>
2024-05-21 12:25:42 +0200

    fix: forbid non-image file in newbill

--- Billed-app-FR-Front/src/containers/NewBill.js
+++ Billed-app-FR-Front/src/containers/NewBill.js
@@ -18,6 +18,12 @@ export default class NewBill {
   handleChangeFile = e => {
     e.preventDefault()
     const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
+    const allowedTypes = ['image/jpg', 'image/jpeg', 'image/png']
+    if (!allowedTypes.includes(file.type)) {
+      const fileInput = this.document.querySelector(`input[data-testid="file"]`)
+      fileInput.value = ""
+      return
+    }
     const filePath = e.target.value.split(/\\/g)
     const fileName = filePath[filePath.length-1]
     const formData = new FormData()
